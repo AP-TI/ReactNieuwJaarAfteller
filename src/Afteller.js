448c34c3af8d9d2d9bc7cdfdcb4418cc8086991e
mixxamm <maximjanssensj5@gmail.com>
2019-12-31 19:42:58 +0100

    Voeg dialog toe
    Autoplay is pas mogelijk na interactie met de pagina.
    Om ervoor te zorgen dat de aftelling zeker afspeelt, moet de gebruiker
    nu eerst op "OK" drukken bij het openen van de pagina.

--- src/Afteller.js
+++ src/Afteller.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
-const geluid = new Audio('../aftellen.wav');
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+const geluid = new Audio('./aftellen.wav');
 
 class Afteller extends Component {
     constructor() {
         super();
 
+
         this.state = {
+            open: true,
             nieuwjaar: new Date(new Date().getFullYear() + 1, 0, 1, 0, 0, 0),
             huidigeTijd: new Date(),
-            vuurwerk: (<div class="pyro">
+            vuurwerk: (<div className="pyro">
                 <div class="before"></div>
                 <div class="after"></div>
             </div>)
         }
     }
 
+    handleClose = () => {
+        this.setState({ open: false });
+    };
     tick = () => {
         this.setState({ huidigeTijd: new Date() });
     }
@@ -26,7 +37,7 @@ class Afteller extends Component {
         s = (s - secs) / 60;
         var mins = s % 60;
         var hrs = (s - mins) / 60;
-        return String(hrs).padStart(2,0) + ':' + String(mins).padStart(2,0) + ':' + String(secs).padStart(2,0);
+        return String(hrs).padStart(2, 0) + ':' + String(mins).padStart(2, 0) + ':' + String(secs).padStart(2, 0);
     }
 
     componentDidMount = () => {
@@ -50,6 +61,27 @@ class Afteller extends Component {
             <div>
                 {(this.state.nieuwjaar - this.state.huidigeTijd) >= 0 ? null : this.state.vuurwerk}
                 <h1>{((this.state.nieuwjaar - this.state.huidigeTijd) >= 0 ? this.milliSecondenNaarTijd(this.state.nieuwjaar - this.state.huidigeTijd) : "Gelukkig Nieuwjaar!")}</h1>
+                <div>
+                    <Dialog
+                        open={this.state.open}
+                        onClose={this.handleClose}
+                        aria-labelledby="alert-dialog-title"
+                        aria-describedby="alert-dialog-description"
+                    >
+                        <DialogTitle id="alert-dialog-title">{"Nieuwjaar aftelklok"}</DialogTitle>
+                        <DialogContent>
+                            <DialogContentText id="alert-dialog-description">
+                                Druk op "OK" om te starten.
+          </DialogContentText>
+                        </DialogContent>
+                        <DialogActions>
+
+                            <Button onClick={this.handleClose} color="primary" autoFocus>
+                                OK
+          </Button>
+                        </DialogActions>
+                    </Dialog>
+                </div>
             </div>
         );
     }
